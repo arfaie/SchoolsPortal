47cb7c586227425ce2883188ea4913a7219ba2f4
Amir Ghasemi <amirghasemi615@gmail.com>
2020-05-03 23:01:44 +0430

    update wwwroot

---
+++ wwwroot/AdminHtml/js/Custom.js
@@ -0,0 +1,36 @@
+﻿function Delete(controller, action) {
+    $(document).on("click", "#Delete", function () {
+        var Row = $(this);
+        var id = $(this).attr('value');
+
+        swal({
+            title: "آیا از حذف این آیتم مطمئن هستید؟",
+            text: "بعد از حذف، امکان بازیابی اطلاعات وجود ندارد!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+            buttons: ['انصراف', 'حذف']
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    $.post('/Admin/' + controller + '/' + action + '/', { id: id },
+                        function (result) {
+                            if (result.status === 'OK') {
+                                window.location.replace("/Admin" + '/' + controller);
+                            }
+                            else {
+                                swal("خطایی در انجام عملیات رخ داد . دوباره تلاش کنید.", {
+                                    icon: "error",
+                                    button: "تایید"
+                                });
+
+                            }
+                        });
+
+                }
+                //else {
+                //    swal("Your user is safe!");
+                //}
+            });
+    });
+}
